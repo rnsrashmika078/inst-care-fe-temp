519b67e686b76157ab61f28e4f54fdd643ed4b8c
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-09-12 14:27:20 +0530

    Revert "redirect to CI auth_api/logout on every logout"
    This reverts commit 2b97a545a8443bd2bb042286a96a5345def95760.

--- src/Components/auth/Logout.jsx
+++ src/Components/auth/Logout.jsx
@@ -1,8 +1,6 @@
-import { HOME_BASE } from "../../config";
-
 export const doFrontendLogout = () => {
-  ["isLoggedIn", "token", "role", "user_id", "technician_id"].forEach((key) =>
-    sessionStorage.removeItem(key),
-  );
-  window.location.href = `${HOME_BASE}/auth_api/logout`;
+  sessionStorage.removeItem("token");
+  sessionStorage.removeItem("role");
+  sessionStorage.removeItem("user_id");
+  sessionStorage.removeItem("technician_id");
 };
